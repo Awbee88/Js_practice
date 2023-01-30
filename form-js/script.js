4605bd5ae519b71818f3b3b3e580fec432988e3a
ilyaLyapin <eximo03@gmail.com>
2023-01-30 12:47:19 +0300

    add delegate

--- form-js/script.js
+++ form-js/script.js
@@ -35,3 +35,13 @@ window.addEventListener('load', function () {
     }
 
 });
+
+function delegate(box, selector, eventName, handler) {
+    box.addEventListener(eventName, function (e) {
+        let elem = e.target.closest(selector);
+
+        if (elem !== null && box.contains(elem)) {
+            handler.call(elem, e);
+        }
+    });
+}
